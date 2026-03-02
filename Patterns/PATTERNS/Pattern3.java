a5e01cfbc8e6a026b710d2fef6cab89dccd67e25
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-03-02 21:35:49 +0530

    Added Pattern3 - basic number pattern using nested loops

---
+++ Patterns/PATTERNS/Pattern3.java
@@ -0,0 +1,20 @@
+package PATTERNS;
+
+public class Pattern3 {
+
+    public static void main(String[] args) {
+       for(int i =0;i<5;i++){
+        for(int j=0;j<5;j++){
+            System.out.print(j+1 + " ");
+        }
+        System.out.println(" ");
+    }
+    }
+}
+/* Pattern output
+    1 2 3 4 5
+    1 2 3 4 5
+    1 2 3 4 5
+    1 2 3 4 5
+    1 2 3 4 5
+ */
