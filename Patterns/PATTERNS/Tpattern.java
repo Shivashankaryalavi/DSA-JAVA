250b082b4e456bf4c194183cbad2d62ab0e14f2f
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-03-04 00:50:00 +0530

    Letter T using row and column boundary conditions

---
+++ Patterns/PATTERNS/Tpattern.java
@@ -0,0 +1,29 @@
+package PATTERNS;
+public class Tpattern{
+    public static void main(String[] args) {
+        for(int i =0;i<=8;i++){
+            for(int j =0;j<=8;j++){
+                if(i==0||j==4){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    
+    }
+}
+/*
+     T Pattern
+    * * * * * * * *            
+          *
+          * 
+          * 
+          * 
+          * 
+          *                
+               
+ */
+
