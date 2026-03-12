9dcef32ab26dbb2e10c8fb508f1173204d27d20a
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-03-12 20:46:11 +0530

    Add java program of addition of matrix

--- Traversal/Addition.java
+++ Traversal/Addition.java
@@ -7,7 +7,7 @@ public class Addition {
         int r1 = sc.nextInt();
         int c1 = sc.nextInt();
         int[][] m1 = new int[r1][c1];
-        System.out.println("Enter a row and column size of m1");
+        System.out.println("Enter a row and column size of m2");
         int r2=sc.nextInt();
         int c2 = sc.nextInt();
         int[][] m2 = new int[r2][c2];
@@ -36,7 +36,8 @@ public class Addition {
             System.out.println("Addition of matrix is :");
             for(int i =0;i<m3.length;i++){
                 for(int j=0;j<m3[i].length;j++){
-                    System.out.print(m3[i][j]+" ");
+                    System.out.printf("%02d",m3[i][j]);
+                    System.out.print(" ");
                 }
                 System.out.println();
             }
---
+++ arrays/basics/Transpose.java
@@ -0,0 +1,19 @@
+package arrays.basics;
+
+public class Transpose {
+
+    public static void main(String[] args) {
+        int[] [] a =  {{1,2} ,{3,4}};
+        int [][] b =  {{2,3},{3,2}};
+
+        for(int i =0;i<a.length;i++){
+            for(int j =0;j<a[i].length;j++){
+                System.out.print(a[i][j]+b[i][j] + " ");
+              
+
+            }
+            System.out.println();
+        
+        }
+    }
+}
