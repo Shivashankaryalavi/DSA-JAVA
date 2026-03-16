bff8df2d354e2a0b661e351dc5b0b68679ff6cb6
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-03-16 22:38:48 +0530

    Add java program to move Zeros to an end

---
+++ ArrayDSA/MoveZerosToEnd.java
@@ -0,0 +1,42 @@
+package ArrayDSA;
+import java.util.Scanner;
+public class MoveZerosToEnd {
+    public static int[] moveZerosToEnd(int[] arr){
+        int size = arr.length;
+        if(size==0 || size==1){
+            return arr;
+        }
+        int nz =0;
+        int z =0;
+        while(nz<size){
+            if(arr[nz]!=1){
+                int temp = arr[nz];
+                arr[nz]=arr[z];
+                arr[z]=temp;
+                nz++;
+                z++;
+            }
+            else{
+                nz++;
+            }
+        }
+        return  arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array elements :");
+        for(int i =0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] result = moveZerosToEnd(arr);
+        for(int i =0;i<result.length;i++){
+            System.out.print(result[i] + " ");
+        }
+        sc.close();
+
+    }
+}
